a30e7611e2015daca0a8a544dc6db137b4de27ff
Antonio Reyes <reytono@gmail.com>
2020-08-05 22:19:30 -0600

    comentarios

--- receptor.py
+++ receptor.py
@@ -1,23 +1,38 @@
 import socket
 import threading
 
+# size del header, es la longitud del contenido que va a enviar el emisor/cliente
 HEADERSIZE = 2020
 FORMAT = "utf-8"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((socket.gethostname(), 1234))
+
+# direccion
+HOST = socket.gethostname()
+
+# puerto donde se va a oir
+PORT = 1234
+
+# el bind() se utiliza para asociar el socket con una interfaz de red específica y un número de puerto:
+server.bind((HOST, PORT))
+
 
 def conexion_cliente(conn, addr):
     print(f"CONEXION CON CLIENTE: {addr}")
 
-    conectado = True
-    while conectado:
+    # mientras que connection sea verdadero, se va a estar en el  while
+    # se va a salir de loop si se envia la palabra adios para ir a close() para cerrar el socket
+    connection = True
+    while connection:
+
+        # conn.rev(HEADERSIZE) se utiliza para recibir cierta cantidad de bytes del cliente
+        # en este caso, queremos recibir la cantidad puesta en HEADERSIZE.
         msg_length = conn.recv(HEADERSIZE).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
 
             if msg == "adios":
-                conectado = False
+                connection = False
 
             print(msg)
 
@@ -26,9 +41,18 @@ def conexion_cliente(conn, addr):
 
 #funcion para iniciar el servidor/receptor
 def iniciar_server():
+    # se oye/busca la conexion del cliente
     server.listen()
+
     while True:
+        # se acepta el .connect() del emison/cliente donde se obtiene 
+        # addr guarda la direccion y puerto de donde vino la conexion 
+        # conn es el objeto que se utilzará para enviar mensajes de vuelta al cliente
         conn, addr = server.accept()
+        # socket
+        print("conn es: ", conn)
+        # se crea un thread para cada nuevo cliente conectado con el servidor para tener más de uno
+        # ejecutará la función conexion_cliente con los argumentos conn y addr
         thread = threading.Thread(target=conexion_cliente, args=(conn, addr))
         thread.start()
 
